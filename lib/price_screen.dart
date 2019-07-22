5bd9823bd1361a8f932ed061427bd4edf2b67dce
bocahrokok <indieunistudy@gmail.com>
2019-07-22 22:45:47 +0700

    commit 22.20

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -13,7 +13,6 @@ class _PriceScreenState extends State<PriceScreen> {
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
-
     for (String currency in currenciesList) {
       var newItem = DropdownMenuItem(
         child: Text(currency),
@@ -22,15 +21,34 @@ class _PriceScreenState extends State<PriceScreen> {
       dropdownItems.add(newItem);
     }
 
-    return DropdownButton(
+    return DropdownButton<String>(
       value: selectedCurrency,
       items: dropdownItems,
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getData();
         });
       },
     );
+
+//    for (String currency in currenciesList) {
+//      var newItem = DropdownMenuItem(
+//        child: Text(currency),
+//        value: currency,
+//      );
+//      dropdownItems.add(newItem);
+//    }
+//
+//    return DropdownButton(
+//      value: selectedCurrency,
+//      items: dropdownItems,
+//      onChanged: (value) {
+//        setState(() {
+//          selectedCurrency = value;
+//        });
+//      },
+//    );
   }
 
   CupertinoPicker IOSPicker() {
@@ -44,7 +62,10 @@ class _PriceScreenState extends State<PriceScreen> {
       backgroundColor: Colors.lightBlue,
       itemExtent: 32.0,
       onSelectedItemChanged: (selectedIndex) {
-        print(selectedIndex);
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+          getData();
+        });
       },
       children: pickerItems,
     );
@@ -57,6 +78,47 @@ class _PriceScreenState extends State<PriceScreen> {
 //      return androidDropdown();
 //    }
 //  }
+
+  Map<String, String> coinValues = {};
+
+  bool isWaiting = false;
+
+  void getData() async {
+    isWaiting = true;
+    try {
+      var data = await CoinData().getCoinData(selectedCurrency);
+      isWaiting - false;
+      setState(() {
+        coinValues = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  Column makeCards(){
+    List<CryptoCard> cryptoCards = [];
+    for(String crypto in cryptoList)
+      cryptoCards.add(
+      cryptoCard(
+        cryptoCurrency : crypto,
+        selectedCurrency: selectedCurrency,
+        value: isWaiting? '?' : coinValues[crypto],
+      ),
+    );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch;
+      children: cryptoCards,
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,5 +161,33 @@ class _PriceScreenState extends State<PriceScreen> {
         ],
       ),
     );
+  }
+}
+
+
+class CryptoCard extends StatelessWidget{
+  const CryptoCard({
+    this.value,
+    this.selectedCurrency,
+    this.cryptoCurrency,
+});
+
+  final String value;
+  final String selectedCurrency;
+  final String cryptoCurrency;
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+
+      )
+    )
   }
 }
